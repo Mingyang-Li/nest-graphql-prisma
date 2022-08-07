3231e64cb8404617db0555365953e13a1e3a9630
Mingyang Li <mingyangli118@gmail.com>
2022-08-07 17:47:43 +1200

    revert: back to use non-absolute path for now

--- src/payment/dtos/PaymentOrderByInput.ts
+++ src/payment/dtos/PaymentOrderByInput.ts
@@ -1,5 +1,5 @@
 import { InputType, Field } from '@nestjs/graphql';
-import { SortOrder } from 'util/SortOrder';
+import { SortOrder } from 'src/util/SortOrder';
 
 @InputType({
   isAbstract: true,
