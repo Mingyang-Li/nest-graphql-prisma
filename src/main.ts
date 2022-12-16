ee3b26aacc79843768f662b27dcdb63f63e5f875
Mingyang-Li <mingyangli118@gmail.com>
2022-12-16 23:09:58 +1300

    👜 updated pkgs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 5000);
-}
+};
 bootstrap();
